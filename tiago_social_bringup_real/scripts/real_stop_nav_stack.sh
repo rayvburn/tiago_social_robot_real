5cd40f4728fe45c45190f57b83923ffba23a3c76
Jarek Karwowski <chromedivizer@gmail.com>
2023-01-23 16:03:53 +0100

    `bringup` - `stop_nav_stack` script: deleted delays, added sourcing of main ROS ws, `vo_server` app not stopped

--- tiago_social_bringup_real/scripts/real_stop_nav_stack.sh
+++ tiago_social_bringup_real/scripts/real_stop_nav_stack.sh
@@ -1,27 +1,26 @@
 #!/usr/bin/env bash
 
-source /opt/pal/ferrum/setup.bash
-DELAY_CALLS=2
+echo "Starting execution on '$HOSTNAME' host"
+
+# on the real robot
+if [ -f "/opt/pal/ferrum/setup.bash" ]
+then
+    source /opt/pal/ferrum/setup.bash
+else
+    # on the development machine
+    source /opt/ros/melodic/setup.bash
+fi
 
 rosservice call /pal_startup_control/stop "app: 'localizer'"
-sleep $DELAY_CALLS
-
 rosservice call /pal_startup_control/stop "app: 'map_configuration_server'"
-sleep $DELAY_CALLS
-
 rosservice call /pal_startup_control/stop "app: 'map_server'"
-sleep $DELAY_CALLS
-
 rosservice call /pal_startup_control/stop "app: 'move_base'"
-sleep $DELAY_CALLS
-
 rosservice call /pal_startup_control/stop "app: 'navigation_sm'"
-sleep $DELAY_CALLS
-
-# app managing /pal_vo_server
-rosservice call /pal_startup_control/stop "app: 'vo_server'"
-sleep $DELAY_CALLS
-
 rosservice call /pal_startup_control/stop "app: 'look_to_cmd_vel'"
+# app managing /pal_vo_server
+# rosservice call /pal_startup_control/stop "app: 'vo_server'"
+
+rosservice call /pal_startup_control/stop "app: 'compressed_map_publisher'"
+rosservice call /pal_startup_control/stop "app: 'poi_navigation'"
 
 echo "Finishing script execution on '$HOSTNAME' host"
