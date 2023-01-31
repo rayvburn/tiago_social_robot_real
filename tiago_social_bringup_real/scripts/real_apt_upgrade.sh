2f7d128e9fc5e6c7ff26ebb5b5263cd8aeb89a64
Jarek Karwowski <chromedivizer@gmail.com>
2023-01-31 21:26:03 +0100

    `bringup` - CMake, pip, catkin, git installation moved from `install_deps` to `apt_upgrade` script
    - this apparently has helped launching custom workspace on the real robot

--- tiago_social_bringup_real/scripts/real_apt_upgrade.sh
+++ tiago_social_bringup_real/scripts/real_apt_upgrade.sh
@@ -11,6 +11,23 @@ sudo add-apt-repository --yes ppa:ubuntu-toolchain-r/test
 sudo apt update
 sudo -H apt-get -y upgrade
 
+# tools
+sudo -H apt-get install -y git
+sudo -H apt-get install -y python-pip
+# NOTE: executing as a non-root user, ref: https://superuser.com/a/1081112
+sudo -i -u $USER_NONROOT bash -c 'echo; echo Installing with pip for user $USER with home at $HOME; pip install --user catkin-tools'
+
+# CMake
+# perl: warning: Falling back to a fallback locale ("en_GB.UTF-8")
+export LC_CTYPE=en_US.UTF-8
+export LC_ALL=en_US.UTF-8
+# sudo apt purge --auto-remove cmake
+sudo -H apt-get install -y software-properties-common
+wget -O - https://apt.kitware.com/keys/kitware-archive-latest.asc 2>/dev/null | gpg --dearmor - | sudo tee /etc/apt/trusted.gpg.d/kitware.gpg >/dev/null
+sudo apt-add-repository 'deb https://apt.kitware.com/ubuntu/ bionic main'
+sudo apt update
+sudo -H apt-get install -y cmake
+
 sleep 1
 echo
 echo "Finishing script on '$HOSTNAME' host"
--- tiago_social_bringup_real/scripts/real_install_deps.sh
+++ tiago_social_bringup_real/scripts/real_install_deps.sh
@@ -16,23 +16,6 @@ sudo -H apt-get install -y curl
 curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
 sudo apt update
 
-# tools
-sudo -H apt-get install -y git
-sudo -H apt-get install -y python-pip
-# NOTE: executing as a non-root user, ref: https://superuser.com/a/1081112
-sudo -i -u $USER_NONROOT bash -c 'echo; echo Installing with pip for user $USER with home at $HOME; pip install --user catkin-tools'
-
-# CMake
-# perl: warning: Falling back to a fallback locale ("en_GB.UTF-8")
-export LC_CTYPE=en_US.UTF-8
-export LC_ALL=en_US.UTF-8
-# sudo apt purge --auto-remove cmake
-sudo -H apt-get install -y software-properties-common
-wget -O - https://apt.kitware.com/keys/kitware-archive-latest.asc 2>/dev/null | gpg --dearmor - | sudo tee /etc/apt/trusted.gpg.d/kitware.gpg >/dev/null
-sudo apt-add-repository 'deb https://apt.kitware.com/ubuntu/ bionic main'
-sudo apt update
-sudo -H apt-get install -y cmake
-
 # Deps acquired from rosdep for perception workspace
 sudo -H apt-get install -y python-scipy
 sudo -H apt-get install -y libnetpbm10-dev
