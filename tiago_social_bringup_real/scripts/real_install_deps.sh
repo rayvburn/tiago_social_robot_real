a2361eca3412c5a9202ac0fe1a4fb3c7503c4897
Jarek Karwowski <chromedivizer@gmail.com>
2023-01-31 21:26:48 +0100

    `bringup` - `install_deps` extended with `EBand` local planner package

--- tiago_social_bringup_real/scripts/real_install_deps.sh
+++ tiago_social_bringup_real/scripts/real_install_deps.sh
@@ -41,6 +41,7 @@ sudo -H apt-get install -y ros-$ROS_DISTRO-people-msgs
 sudo -H apt-get install -y ros-$ROS_DISTRO-laser-filters
 sudo -H apt-get install -y ros-$ROS_DISTRO-navigation-layers
 sudo -H apt-get install -y ros-$ROS_DISTRO-diagnostic-updater
+sudo -H apt-get install -y ros-$ROS_DISTRO-eband-local-planner
 
 # HuBeRo local planner dependency
 # NOTE1: Ubuntu 18 does not have 6.0 version of the library available as .deb package
