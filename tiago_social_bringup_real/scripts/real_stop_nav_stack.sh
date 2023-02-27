924907e61208e3e6e7265c22c7453d11f05dd0f6
Jarek Karwowski <chromedivizer@gmail.com>
2023-02-27 16:27:52 +0100

    `bringup` - `stop_nav_stack` - head manager stopped

--- tiago_social_bringup_real/scripts/real_stop_nav_stack.sh
+++ tiago_social_bringup_real/scripts/real_stop_nav_stack.sh
@@ -22,5 +22,6 @@ rosservice call /pal_startup_control/stop "app: 'look_to_cmd_vel'"
 
 rosservice call /pal_startup_control/stop "app: 'compressed_map_publisher'"
 rosservice call /pal_startup_control/stop "app: 'poi_navigation'"
+rosservice call /pal_startup_control/stop "app: 'head_manager'"
 
 echo "Finishing script execution on '$HOSTNAME' host"
