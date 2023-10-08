0b2eae822384eb24000f3e916b498dedc6ed9b66
Jarek Karwowski <chromedivizer@gmail.com>
2023-10-08 15:21:03 +0200

    `bringup` - added a usage note in `prepare_configured_robot` script

--- tiago_social_bringup_real/scripts/prepare_configured_robot.sh
+++ tiago_social_bringup_real/scripts/prepare_configured_robot.sh
@@ -2,6 +2,8 @@
 #
 # Prepare environment for a robot that has a proper workspace already built
 #
+# You are safe to run this once the Web Commander is set up properly (do not try this earlier)
+#
 cd $(rospack find tiago_social_bringup_real)/scripts
 ./script_via_ssh.sh real_stop_nav_stack.sh pal pal
 ./script_via_ssh.sh real_install_deps.sh root palroot
