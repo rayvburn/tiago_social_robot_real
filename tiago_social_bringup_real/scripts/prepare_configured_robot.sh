744f1a9e1adb110f59f84d2d11634376b1b9ead1
rayvburn <chromedivizer@gmail.com>
2023-12-22 11:44:24 +0100

    `bringup` - scripts to prepare (un)configured robot for operation do not need to be called from a terminal with a sourced workspace

--- tiago_social_bringup_real/scripts/prepare_configured_robot.sh
+++ tiago_social_bringup_real/scripts/prepare_configured_robot.sh
@@ -8,6 +8,9 @@
 # might as well be a hostname but giving IP here is more robust
 ROBOT_IP="${1:-192.168.18.66}"
 
-cd $(rospack find tiago_social_bringup_real)/scripts
+# Ref: https://stackoverflow.com/a/246128
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+cd $SCRIPT_DIR
 ./script_via_ssh.sh real_stop_nav_stack.sh pal pal $ROBOT_IP
 ./script_via_ssh.sh real_install_deps.sh root palroot $ROBOT_IP
